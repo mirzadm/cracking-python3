973fa3803ab4668e70c078417f48f441d5dd27d0
Mirzad Mohandespour <mirzadm@gmail.com>
2018-06-28 12:51:18 -0500

    q0108: update & add tests

--- src/q0108.py
+++ src/q0108.py
@@ -1,12 +1,23 @@
-"""Are two strings rotations of one another?"""
+"""Chapter 1: Question 8.
+
+Assume you have a method isSubstring which checks if one word is a substring
+of another. Given two strings, s1 and s2, write code to check if s2 is a
+rotation of s1 using only one call to isSubstring
+
+Example: "waterbottle" is a rotation of "erbottlewat"
+"""
 
 
 def is_rotation_slicing(s1, s2):
+    """Uses slicing."""
+    if not s1:
+        return False
+
     n = len(s1)
     if n != len(s2):
         return False
-    
-    for i in range(0, n):
+
+    for i in range(n):
         if s1[i:n] + s1[0:i] == s2:
             return True
 
@@ -14,13 +25,13 @@ def is_rotation_slicing(s1, s2):
 
 
 def is_rotation_substring(s1, s2):
+    """Uses substring method."""
+    if not s1:
+        return False
+
     n = len(s1)
     if n != len(s2):
         return False
 
     s = s1 + s1
     return s2 in s
-
-
-# print(is_rotation_slicing('happy', 'ppyha'))
-
---
+++ tests/test_q0108.py
@@ -0,0 +1,42 @@
+"""Unit tests."""
+
+import unittest
+
+from src.q0108 import is_rotation_slicing, is_rotation_substring
+
+
+class TestIsRotation(unittest.TestCase):
+    """Tests for two implementations of is_rotation."""
+    def test_is_rotation_slicing(self):
+        self.assertFalse(is_rotation_slicing('', ''))
+        self.assertFalse(is_rotation_slicing('', 'a'))
+        self.assertFalse(is_rotation_slicing('a', 'b'))
+        self.assertTrue(is_rotation_slicing('a', 'a'))
+        self.assertFalse(is_rotation_slicing('a', 'aa'))
+        self.assertTrue(is_rotation_slicing('ab', 'ab'))
+        self.assertTrue(is_rotation_slicing('ab', 'ba'))
+        self.assertTrue(is_rotation_slicing('abc', 'cab'))
+        self.assertTrue(is_rotation_slicing('abc', 'bca'))
+        self.assertTrue(is_rotation_slicing('abc', 'abc'))
+        self.assertFalse(is_rotation_slicing('abc', 'cba'))
+        self.assertFalse(is_rotation_slicing('abc', 'bac'))
+        self.assertFalse(is_rotation_slicing('abc', 'acb'))
+
+    def test_is_rotation_substring(self):
+        self.assertFalse(is_rotation_substring('', ''))
+        self.assertFalse(is_rotation_substring('', 'a'))
+        self.assertFalse(is_rotation_substring('a', 'b'))
+        self.assertTrue(is_rotation_substring('a', 'a'))
+        self.assertFalse(is_rotation_substring('a', 'aa'))
+        self.assertTrue(is_rotation_substring('ab', 'ab'))
+        self.assertTrue(is_rotation_substring('ab', 'ba'))
+        self.assertTrue(is_rotation_substring('abc', 'cab'))
+        self.assertTrue(is_rotation_substring('abc', 'bca'))
+        self.assertTrue(is_rotation_substring('abc', 'abc'))
+        self.assertFalse(is_rotation_substring('abc', 'cba'))
+        self.assertFalse(is_rotation_substring('abc', 'bac'))
+        self.assertFalse(is_rotation_substring('abc', 'acb'))
+
+
+if __name__ == '__main__':
+    unittest.main()
