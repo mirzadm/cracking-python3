72e2fc4ff1981c2be84d9cd8c7578fab31d46b3b
Mirzad Mohandespour <mirzadm@gmail.com>
2018-06-28 10:03:57 -0500

    q0106: modify & add tests

--- src/q0106.py
+++ src/q0106.py
@@ -1,37 +1,57 @@
-"""90 degree rotation of a pixel matrix."""
+"""90 degree rotation of a pixel matrix.
+
+Rotates a pixel matrix 90 degress clockwise.
+"""
 
 
-def rotate_matrix(matrix, n):
-    rotated = [[0]*n for _ in range(n)]
-    
+def rotate_matrix_90_clk(matrix):
+    """Creates a new rotated matrix.
+
+    Deos not change the input matrix.
+    Args:
+        A square 2-dim matrix.
+    Returns:
+        Rotated matrix.
+    """
+    if not matrix:
+        return []
+
+    n = len(matrix)
+    if n == 1:
+        return [matrix[0]]
+
+    rotated_matrix = [[0] * n for _ in range(n)]
     for row in range(n):
         for col in range(n):
-            rotated[row][col] = matrix[n-1-col][row]
-    
-    return rotated
+            rotated_matrix[col][n - 1 - row] = matrix[row][col]
+
+    return rotated_matrix
 
 
-# Rotate in-place.
-def rotate_matrix_inplace(matrix, n):
-    for j in range(n-1):
-        row = 0
-        col = j
-        current = matrix[row][col]
-        for k in range(4): 
-            next_row = col
-            next_col = n - 1 - row
-            backup = matrix[next_row][next_col]
-            matrix[next_row][next_col] = current
-            row = next_row
-            col = next_col
-            current = backup
+def rotate_matrix__90_clk_inplace(matrix):
+    """In-place rotation. Changes the input matrix.
+
+    Args:
+        A square 2-dim matrix.
+    Returns:
+        Rotated matrix (in-place)
+    """
+    n = len(matrix)
+    if n <= 1:
+        return matrix
+
+    for i in range(n//2):
+        for j in range(i, n - i - 1):
+            row = i
+            col = j
+            current = matrix[row][col]
+            for k in range(4):
+                next_row = col
+                next_col = n - 1 - row
+                backup = matrix[next_row][next_col]
+                matrix[next_row][next_col] = current
+                row = next_row
+                col = next_col
+                current = backup
 
     return matrix
-
-
-
-#   1   2   3
-#   4   5   6
-#   7   8   9
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(rotate_matrix_inplace(matrix, 3))
---
+++ tests/test_q0106.py
@@ -0,0 +1,63 @@
+"""Unit tests."""
+
+import unittest
+
+from src.q0106 import (rotate_matrix_90_clk as rotate_matrix,
+                       rotate_matrix__90_clk_inplace as rotate_matrix_inplace)
+
+
+class Test(unittest.TestCase):
+    """Tests for matrix 90 degress clockwise rotation."""
+    def setUp(self):
+        self.m22 =   [[1, 2],
+                      [3, 4]]
+
+        self.m22_r = [[3, 1],
+                      [4, 2]]
+
+        self.m33 =   [[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9]]
+
+        self.m33_r = [[7, 4, 1],
+                      [8, 5, 2],
+                      [9, 6, 3]]
+
+        self.m44 =   [[1,   2,  3,  4],
+                      [5,   6,  7,  8],
+                      [9,   10, 11, 12],
+                      [13,  14, 15, 16]]
+
+        self.m44_r = [[13,  9,  5,  1],
+                      [14,  10, 6,  2],
+                      [15,  11, 7,  3],
+                      [16,  12, 8,  4]]
+
+    def test_rotate_matrix(self):
+        self.assertEqual(rotate_matrix([]),[])
+        self.assertEqual(rotate_matrix([1]),[1])
+        self.assertEqual(rotate_matrix(self.m22), self.m22_r)
+        self.assertEqual(rotate_matrix(self.m33), self.m33_r)
+        self.assertEqual(rotate_matrix(self.m44), self.m44_r)
+        # 4 rotations yield the same matrix
+        self.assertEqual(rotate_matrix(
+                            rotate_matrix(
+                                rotate_matrix(
+                                    rotate_matrix(self.m44)))), self.m44)
+
+    def test_rotate_matrix_inplace(self):
+        self.assertEqual(rotate_matrix_inplace([]), [])
+        self.assertEqual(rotate_matrix_inplace([1]), [1])
+        self.assertEqual(rotate_matrix_inplace(self.m22), self.m22_r)
+        self.assertEqual(rotate_matrix_inplace(self.m33), self.m33_r)
+        self.assertEqual(rotate_matrix_inplace(self.m44), self.m44_r)
+        # 4 rotations yield the same matrix
+        self.assertEqual(rotate_matrix_inplace(
+                            rotate_matrix_inplace(
+                                rotate_matrix_inplace(
+                                    rotate_matrix_inplace(self.m44)))),
+                                                          self.m44)
+
+
+if __name__ == '__main__':
+    unittest.main()
