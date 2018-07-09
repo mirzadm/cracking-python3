eb5f8d02671ecdeecd9417a833cb0ab952fadecb
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-08 22:05:35 -0500

    q0302: update

--- src/q0302.py
+++ src/q0302.py
@@ -1,58 +1,33 @@
-"""A stack with O(1) min method."""
+"""Chapter 3: Question 2.
 
+Implement a stack with an O(1) min() method.
+"""
 
-class Stack:
-    
-    def __init__(self):
-        self.stack = []
-    
-    def push(self, item):
-        self.stack.append(item)
-
-    def pop(self):
-        if self.stack == []:
-            return None
-        else:
-            return self.stack.pop()
-
-    def peek(self):
-        if self.stack == []:
-            return None
-        else:
-            return self.stack[-1]
+from src.utils.stack import Stack
 
 
 class StackWithMin(Stack):
+    """Uses a secondary stack to keep track of minimum values."""
 
     def __init__(self):
         super().__init__()
         self.min_stack = Stack()
 
     def push(self, item):
-        super().push(item)
-        min_value = self.min()
-        if min_value is None:
+        if self.is_empty():
             self.min_stack.push(item) 
         elif item <= self.min():
             self.min_stack.push(item)
+        super().push(item)
 
     def pop(self):
-        item = super().pop()
-        if item is not None:
-            if self.min() == item:
-                self.min_stack.pop()
-
-        return item
+        item = super().peek() # Throws an exception if empty
+        if self.min() == item:
+            self.min_stack.pop()
+        return super().pop()
 
     def min(self):
-        return self.min_stack.peek()
-
-
-# s = StackWithMin()
-# a = [1, 2, 3, 1, 0, 4]
-# for i in a:
-#     s.push(i)
-
-# while s.peek() is not None:
-#     print('min: {}  peek: {}'.format(s.min(), s.peek()))
-#     s.pop()
+        if self.is_empty():
+            raise IndexError('Stack is empty.')
+        else:
+            return self.min_stack.peek()
