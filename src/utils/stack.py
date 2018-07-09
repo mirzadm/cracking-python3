7a3f889ea2a9f7599f7414f99ec96678a5da4bd4
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-08 20:33:01 -0500

    basic stack

---
+++ src/utils/stack.py
@@ -0,0 +1,26 @@
+"""Stack implementation."""
+
+
+class Stack:
+    """Simple stack implementation using list."""
+
+    def __init__(self):
+        self.stack_list = []
+
+    def push(self, item):
+        self.stack_list.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError('Stack is empty.')
+        else:
+            return self.stack_list.pop()
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def is_empty(self):
+        return self.stack_list == []
