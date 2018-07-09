f2205f39ebe9243eda666831d19ddc7df6857045
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-08 20:47:23 -0500

    add tests for peek()

--- tests/test_utils/test_stack.py
+++ tests/test_utils/test_stack.py
@@ -10,26 +10,39 @@ class TestStack(unittest.TestCase):
         self.s = Stack()
 
     def test_init(self):
-        self.assertEqual(self.s.stack_list, [])
-
-    def test_is_empty(self):
         self.assertTrue(self.s.is_empty())
 
-    def test_push_pop(self):
+    def test_push_pop_peek_empty(self):
         self.assertRaises(IndexError, self.s.pop)
 
+        self.assertTrue(self.s.is_empty())
         self.s.push(1)
+        self.assertFalse(self.s.is_empty())
+        self.assertEqual(self.s.peek(), 1)
         self.assertEqual(self.s.pop(), 1)
         self.assertTrue(self.s.is_empty())
+        self.assertRaises(IndexError, self.s.peek)
+        self.assertRaises(IndexError, self.s.pop)
+
 
         self.s.push(1)
+        self.assertFalse(self.s.is_empty())
         self.s.push(2)
+        self.assertFalse(self.s.is_empty())
         self.s.push(3)
+        self.assertFalse(self.s.is_empty())
+        self.assertEqual(self.s.peek(), 3)
+        self.assertFalse(self.s.is_empty())
         self.assertEqual(self.s.pop(), 3)
+        self.assertFalse(self.s.is_empty())
+        self.assertEqual(self.s.peek(), 2)
+        self.assertFalse(self.s.is_empty())
         self.assertEqual(self.s.pop(), 2)
+        self.assertFalse(self.s.is_empty())
+        self.assertEqual(self.s.peek(), 1)
+        self.assertFalse(self.s.is_empty())
         self.assertEqual(self.s.pop(), 1)
         self.assertTrue(self.s.is_empty())
-
         self.assertRaises(IndexError, self.s.pop)
 
 
