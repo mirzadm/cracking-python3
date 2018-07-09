7a422d52e494a6f3ad2fe67ebd64c798674c35a7
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-09 10:04:46 -0500

    q0304: update

--- src/q0304.py
+++ src/q0304.py
@@ -1,78 +1,21 @@
-"""Towers of Hanoi."""
+"""Chapter 3: Question 4.
+
+Towers of Hanoi.
+"""
 
 
-def hanoi_towers(n ,s1, s2, s3):
+def towers_of_hanoi(n ,s1, s2, s3):
+    """Moves n plates from s1 to s3.
+
+    Assumes there exist n plates on s1.
+    """
+
     if n == 1:
         s2.push(s1.pop())
         s3.push(s2.pop())
     else:   
-        hanoi_towers(n-1, s1, s2, s3)
+        towers_of_hanoi(n-1, s1, s2, s3)
         s2.push(s1.pop())
-        hanoi_towers(n-1, s3, s2, s1)
+        towers_of_hanoi(n-1, s3, s2, s1)
         s3.push(s2.pop())
-        hanoi_towers(n-1, s1, s2, s3)
-
-
-class Node:
-    def __init__(self):
-        self.value = None
-        self.next = None
-
-
-class Stack:
-    
-    def __init__(self):
-        self.top = None
-
-    def push(self, item):
-        n = Node()
-        n.value = item
-        if self.is_empty():
-            self.top = n
-        else:
-            n.next = self.top
-            self.top = n
-
-    def pop(self):
-        if self.is_empty():
-            return None
-        else:
-            t = self.top.value
-            self.top = self.top.next
-            return t
-    
-    def peek(self):
-        if self.is_empty():
-            return None
-        else:
-            return self.top.value
-
-    def is_empty(self):
-        return self.top is None 
-
-
-def test():
-    a = list(range(10, 0, -1))
-    s1 = Stack()
-    s2 = Stack()
-    s3 = Stack()
-    for i in a:
-        s1.push(i)
-
-    hanoi_towers(5, s1, s2, s3)
-    print('Stack 1')
-    while not s1.is_empty():
-        print(s1.pop())
-    
-    print('Stack 2')
-    while not s2.is_empty():
-        print(s2.pop())
-
-    print('Stack 3')
-    while not s3.is_empty():
-        print(s3.pop())
-
-
-if __name__ == '__main__':
-    test()
-
+        towers_of_hanoi(n-1, s1, s2, s3)
