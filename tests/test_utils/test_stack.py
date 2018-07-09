3ceacc9c8063ff16f6bf6e3ddb46fc589c157237
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-08 21:28:53 -0500

    rewrite tests

--- tests/test_utils/test_stack.py
+++ tests/test_utils/test_stack.py
@@ -11,39 +11,35 @@ class TestStack(unittest.TestCase):
 
     def test_init(self):
         self.assertTrue(self.s.is_empty())
+        self.assertEqual(self.s.size(), 0)
 
-    def test_push_pop_peek_empty(self):
+    def test_push_and_size(self):
+        no_of_pushes = 10
+        for i in range(no_of_pushes):
+            self.s.push(i)
+        self.assertFalse(self.s.is_empty())
+        self.assertEqual(self.s.size(), no_of_pushes)
+
+    def test_push_then_pop(self):
+        no_of_pushes = 10
+        for i in range(no_of_pushes):
+            self.s.push(i)
+        for i in range(no_of_pushes):
+            self.assertEqual(self.s.pop(), no_of_pushes - i - 1)        
+        self.assertTrue(self.s.is_empty())
+        self.assertEqual(self.s.size(), 0)
+
+    def test_push_then_peek(self):
+        self.s.push(100)
+        size = self.s.size()
+        self.assertEqual(self.s.peek(), 100)
+        self.assertEqual(self.s.size(), size)
+
+    def test_pop_from_empty_stack(self):
         self.assertRaises(IndexError, self.s.pop)
 
-        self.assertTrue(self.s.is_empty())
-        self.s.push(1)
-        self.assertFalse(self.s.is_empty())
-        self.assertEqual(self.s.peek(), 1)
-        self.assertEqual(self.s.pop(), 1)
-        self.assertTrue(self.s.is_empty())
+    def test_peek_into_empty_stack(self):
         self.assertRaises(IndexError, self.s.peek)
-        self.assertRaises(IndexError, self.s.pop)
-
-
-        self.s.push(1)
-        self.assertFalse(self.s.is_empty())
-        self.s.push(2)
-        self.assertFalse(self.s.is_empty())
-        self.s.push(3)
-        self.assertFalse(self.s.is_empty())
-        self.assertEqual(self.s.peek(), 3)
-        self.assertFalse(self.s.is_empty())
-        self.assertEqual(self.s.pop(), 3)
-        self.assertFalse(self.s.is_empty())
-        self.assertEqual(self.s.peek(), 2)
-        self.assertFalse(self.s.is_empty())
-        self.assertEqual(self.s.pop(), 2)
-        self.assertFalse(self.s.is_empty())
-        self.assertEqual(self.s.peek(), 1)
-        self.assertFalse(self.s.is_empty())
-        self.assertEqual(self.s.pop(), 1)
-        self.assertTrue(self.s.is_empty())
-        self.assertRaises(IndexError, self.s.pop)
 
 
 if __name__ == '__main__':
