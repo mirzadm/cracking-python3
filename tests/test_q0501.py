f2f45c6430fc97f0be30a8441524ad61edff1816
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-12 23:34:10 -0500

    q0501: add tests

--- tests/test_q0501.py
+++ tests/test_q0501.py
@@ -1,17 +1,35 @@
-"""Unit tests for q0501."""
+"""Unit tests for q0501.py."""
 
 import unittest
 
-from src.q0501 import insert_bits as insert_bits
+from src.q0501 import insert_bits_in_num
 
 
-class MyTests(unittest.TestCase):
+class MyInsertBits(unittest.TestCase):
 
-    def test_insert(self):
-        self.assertTrue(insert_bits(0b11111111, 0b010, 3, 1) == 0b11110101)
-        self.assertTrue(insert_bits(0b11111111, 0b010, 4, 1) == 0b11100101)
-        self.assertTrue(insert_bits(0b11111111, 0b010, 7, 5) == 0b01011111)
+    def test_insert_one_bit_1(self):
+        m = 0b1
+        n = 0b0000_0000
+        i = 0
+        j = 0
+        m_in_n = 0b0000_0001
+        self.assertEqual(insert_bits_in_num(m, n, i, j), m_in_n)
 
+    def test_insert_one_bit_0(self):
+        m = 0b0
+        n = 0b0000_0001
+        i = 0
+        j = 0
+        m_in_n = 0b0
+        self.assertEqual(insert_bits_in_num(m, n, i, j), m_in_n)
+
+    def test_insert_two_bits_11(self):
+        m = 0b11
+        n = 0b0000_0000
+        i = 2
+        j = 3
+        m_in_n = 0b0000_1100
+        self.assertEqual(insert_bits_in_num(m, n, i, j), m_in_n)
 
 if __name__ == '__main__':
     unittest.main()
