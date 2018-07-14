02bde1f0f38377afdcd36d04e844d72fd339d175
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-14 12:00:01 -0500

    q0503: correct solution

--- src/q0503.py
+++ src/q0503.py
@@ -1,50 +1,54 @@
-"""Find the next smallest and biggest integers with same number of 1 bits."""
+"""Chapter 5: Question 3.
+
+Find the next smallest and biggest integers with same number of 1 bits.
+Solution given for next biggest. Next smallest follows the same logic.
+"""
 
 
-def find_next_same_bits(n):
-    k = 0
-    m = abs(n)
-    seq01 = None
-    seq10 = None
-    while m > 0 and (seq01 is None or seq10 is None):
-        right = m & 1
-        next = m & 2
-        if seq10 is None:
-            if next != 0 and right == 0:
-                seq10 = k
+def find_next_biggest_with_same_1s(n):
+    """Finds the next biggest number with the same number of 1 bits.
 
-        if seq01 is None:
-            if next == 0 and right != 0:
-                seq01 = k
+    - Flips the rightmost 0 that has ones on its right (increases the value)
+    - Rearrange 1s on its right to lowest positions and flips highest of them
+      (decreases the value and creates same number of 1s)
 
-        k += 1
-        m = m >> 1
-        
-    next_big = None
-    next_small = None
-    if seq10 is not None:
-        next_big = abs(n) ^ (0b11 << seq10)
-    if seq01 is not None:
-        next_small = abs(n) ^ (0b11 << seq01)
+    Example:
+    xxxx_0_111_0000 --> xxxx_1_111_0000 --> xxxx_1_000_0011
 
-    if n > 0: 
-        return (next_big, next_small)
-    else:
-        return (-next_small, -next_big)
+    Args:
+        n: A positive integer.
+    Raises:
+        ValueError on non-positive input.
+    Returns:
+        Next biggest number with same number of 1s.
+    """
 
+    if n <= 0:
+        raise ValueError('Input argument has to be positive.')
 
-def test():
-    n = -0b10
-    next_biggest, next_smallest = find_next_same_bits(n)
+    temp = n
 
-    print('n: {0:b}'.format(n))
- 
-    if next_biggest is not None:
-        print('next biggest: {0:b}'.format(next_biggest))
+    # Count number rightmost 0s 
+    num_of_zeros = 0
+    while temp & 1 == 0:
+        temp >>= 1
+        num_of_zeros += 1
 
-    if next_smallest is not None:
-        print('next smallest: {0:b}'.format(next_smallest))
+    # Count number of 1s to the left of 0s
+    num_of_ones = 0
+    while temp & 1 == 1:
+        temp >>= 1
+        num_of_ones += 1
 
+    # Flip next 0 to 1
+    n = n ^ (1 << (num_of_ones + num_of_zeros))
 
-if __name__ == '__main__':
-    test()
+    # Create a 0...01...1 mask, then invert it to get 1...10...0
+    mask = ~((1 << (num_of_ones + num_of_zeros)) - 1)
+    n = n & mask
+
+    # Create a 0...01...1 mask with number of 1s = (num_of_ones - 1)
+    mask = (1 << (num_of_ones - 1)) - 1
+    n = n | mask
+
+    return n
