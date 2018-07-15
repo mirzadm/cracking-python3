c88d5a81bd7191b90c9141e3d9058486d0d18624
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-15 16:14:37 -0500

    q0901: update

--- src/q0901.py
+++ src/q0901.py
@@ -1,6 +1,14 @@
-"""Number of ways to run up a staircase."""
+"""Chapter 9: Problem 1.
 
-def count_ways_rec(n):
+How many ways to run up n stairs. If you could hop 1, 2, or 3 steps.
+"""
+
+
+def run_up_stairs_rec(n):
+    """Counts number of ways to run up stairs.
+    
+    Inefficient solution with O(3^n) time.
+    """
     if n <= 0:
         return 0
     if n == 1:
@@ -10,10 +18,14 @@ def count_ways_rec(n):
     if n == 3:
         return 4
 
-    return count_ways_rec(n-1) + count_ways_rec(n-2) + count_ways_rec(n-3)
+    return run_up_stairs_rec(n-1) + run_up_stairs_rec(n-2) + run_up_stairs_rec(n-3)
 
 
-def count_ways_iter(n):
+def run_up_stairs_iter(n):
+    """Counts number of ways to run up stairs.
+    
+    More efficient solution with O(n) time.
+    """
     if n <= 0:
         return 0
     if n == 1:
@@ -23,18 +35,15 @@ def count_ways_iter(n):
     if n == 3:
         return 4
 
-    p = [4, 2, 1]
+    k_1 = 4
+    k_2 = 2
+    k_3 = 1
     i = 4
     while i <= n:
-        temp = p[0] + p[1] + p[2]
-        p[2] = p[1]
-        p[1] = p[0]
-        p[0] = temp
+        ith_sum = k_1 + k_2 + k_3
+        k_3 = k_2
+        k_2 = k_1
+        k_1 = ith_sum
         i += 1
 
-    return p[0]
-
-
-# n = 100 # too high for recursion
-# print('Number of ways for {} stairs with iteration and recursion:')
-# print(count_ways_iter(n))
+    return ith_sum
